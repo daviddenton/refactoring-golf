c185c77b0f2df831db92afa35385829983ec9b9d
magazinews1 <magazinews1@gmail.com>
2017-02-23 10:46:19 +0000

    prepare hole6 workspace

---
+++ hole7/src/main/java/hole7/TaxRate.java
@@ -0,0 +1,22 @@
+package hole7;
+
+public class TaxRate {
+    private final int percent;
+
+    private TaxRate(int percent) {
+        this.percent = percent;
+    }
+
+    public static TaxRate taxRate(int percent) {
+        return new TaxRate(percent);
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    Money apply(Money total) {
+        Double amount = total.value * (getPercent() / 100d);
+        return Money.money(amount.intValue(), total.currency);
+    }
+}
---
+++ hole7/src/test/java/hole7/TakeHomeCalculatorTest.java
@@ -0,0 +1,19 @@
+package hole7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TakeHomeCalculatorTest {
+
+    @Test
+    public void canCalculateTax() throws Exception {
+        Integer first = new TakeHomeCalculator(TaxRate.taxRate(10)).netAmount(Money.money(40, "GBP"), Money.money(50, "GBP"), Money.money(60, "GBP")).value;
+        assertEquals(135, first.intValue());
+    }
+
+    @Test(expected = Incalculable.class)
+    public void cannotSumDifferentCurrencies() throws Exception {
+        new TakeHomeCalculator(TaxRate.taxRate(10)).netAmount(Money.money(4, "GBP"), Money.money(5, "USD"));
+    }
+}
